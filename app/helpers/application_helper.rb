68a851547336b5a5e9cf2050fa0f85cab49912b7
Kapcsándi István <pisti@pisti-laptop.(none)>
2009-10-31 21:06:45 +0100

    cart items

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,10 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
   def cart_items
-    if session[:cart].nil?
-      0
-    else
+    if session[:cart]
       session[:cart].items.count
+    else
+      0
     end
   end
 end
