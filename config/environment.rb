260cecf676b564abb7193d34ddff29274c08a1ec
Kapcsándi István <istvan.kapcsandi@gmail.com>
2009-11-09 11:35:56 +0100

    calendar

--- config/environment.rb
+++ config/environment.rb
@@ -20,12 +20,12 @@ Rails::Initializer.run do |config|
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
 
-  config.gem "calendar_helper"
   config.gem "authlogic"
   config.gem "searchlogic"
   config.gem "calendar_date_select"
   config.gem "validates_timeliness"
   config.gem "RedCloth", :lib => 'redcloth'
+  config.gem "calendar_helper"
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
