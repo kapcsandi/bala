73811f7abe593ed8eead93ae90b4ab4fd5b1d167
Kapcsándi István <pisti@pisti-laptop.(none)>
2009-11-11 03:02:59 +0100

    search breadcrumb

--- app/helpers/layout_helper.rb
+++ app/helpers/layout_helper.rb
@@ -22,22 +22,35 @@ module LayoutHelper
   end
   
   def breadcrumb
-    html = []
+    html, param = [], {}
     html << link_to(t(:home), '/') unless controller_name == 'root' and action_name == 'index'
     if controller_name == 'houses'
-      if params[:advanced] == '1'
-        html << link_to(t(:detailed_search), houses_path( :advanced => '1')) 
-      elsif params[:q]
+      if params[:advanced]
+				param.merge!({ :advanced => '' })
+        html << link_to(t(:detailed_search), houses_path( param ))
+      elsif params[:commit]
         html << link_to(t(:search), houses_path) 
 			end
 			if params[:discount]
-        html << link_to(t(:special_offers), special_offers_path)
+        html << link_to(t(:special_offers), special_offers_path(param))
       elsif params[:category]
-        html << link_to(params[:category], houses_path(:category => params[:category]))
+        html << link_to(params[:category], houses_path(param.merge!(:category => params[:category])))
       end
-      if params[:q] and params[:q][:where]
-        html << link_to(params[:q][:where], houses_path( 'q[where]' => params[:q][:where])) 
+			where = (params[:commit].nil? or params[:q][:where].nil? or params[:q][:where].empty?) ? nil : params[:q][:where]
+      if where
+        html << link_to(where, houses_path(param.merge!( 'q[where]' => where)))
       end
+				house_type = (params[:commit].nil? or params[:q][:type].nil? or params[:q][:type].empty?) ? nil : params[:q][:type]
+				if house_type
+        html << link_to(house_type, houses_path(param.merge!( 'q[type]' => house_type)))
+			end
+			if action_name == 'show'
+				html << link_to(t(:search), houses_path(param))
+				where = @content_for_title.split(', ')[-1]
+        html << link_to(where, houses_path(param.merge!( 'q[where]' => where)))
+				house_type = @content_for_title.split(', ')[0]
+        html << link_to(house_type, houses_path(param.merge!( 'q[house_type]' => house_type)))
+			end
     end
     html << @content_for_title #if show_title?
     t('you_are_here') + html.join(' > ')
