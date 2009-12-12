0adf6630c214bcf0a40ee282debb42a72b14f965
Kapcsándi István <pisti@pisti-laptop.(none)>
2009-12-12 13:10:48 +0100

    cleanup

--- app/models/season.rb
+++ app/models/season.rb
@@ -1,7 +1,8 @@
 class Season < ActiveRecord::Base
   attr_accessible :name, :start, :end
+  default_scope :order => 'start ASC'
 
-  named_scope :next_year, {:conditions => "end > #{Date.today.strftime}", :order => 'start ASC'}
+  named_scope :next_year, {:conditions => "end > #{Date.today.strftime}"}
 
   def self.which_season?(day)
     self.next_year.select { |season| (day >= season.start) and (day <= season.end) }[0].name
