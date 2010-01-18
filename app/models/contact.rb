19a65add4900f3a67d3510078631df980a764fa8
Kapcsándi István <pisti@pisti-laptop.(none)>
2010-01-19 00:16:04 +0100

    contact

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -12,7 +12,7 @@ class Contact  < ActiveRecord::BaseWithoutTable
   column :children, :string
   column :with_animals, :boolean
   column :notes, :string
-  validates_presence_of :house_id, :email
+  validates_presence_of :email
   validates_format_of :email, :with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i
   validates_length_of :email, :within => 5..255
 
