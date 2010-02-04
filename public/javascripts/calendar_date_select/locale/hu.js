56250abc6252c6f5bd966e66a7d0c9566a10764f
Kapcsándi István <istvan.kapcsandi@gmail.com>
2010-02-04 13:33:48 +0000

    hu js fix

--- public/javascripts/calendar_date_select/locale/hu.js
+++ public/javascripts/calendar_date_select/locale/hu.js
@@ -1,6 +1,6 @@
 Date.weekdays = $w('Hé Ke Sze Cs P Szo Va');
 Date.months = $w('Január Február Március Április Május Június Július Augusztus Szeptember Október November December');
-Date.short_date_format = 'yyyy-MM-dd';
+Date.short_date_format = 'yyyy.MM.dd';
 Date.first_day_of_week = 1;
 
 _translations = {
